fc96c77794929293c66cd7a199229e081a33ba77
Tim F <fairdogJR@users.noreply.github.com>
2022-09-30 10:26:52 -0400

    Add files via upload
    Added help file info and some try except for cases

--- U1816EExample.py
+++ U1816EExample.py
@@ -1,37 +1,48 @@
 #Tim Fairfield Sept 22,2022
-import subprocess
+#https://github.com/fairdogJR/u1816enetframtest
 
-#makesure that testswitchdll.exe and the Agilent.U1816x are in this dir
-appPath=r'C:\Users\fairfiel.KEYSIGHT\source\repos\testswitchdll\bin\Debug\netcoreapp3.1\testswitchdll.exe'
+import subprocess
+import sys
+#makesure that exe and the Agilent.U1816x are in the dir with this tool
+appPath=r'C:\Users\fairfiel.KEYSIGHT\source\repos\testswitchdll\bin\Debug\netcoreapp3.1\U1816ESwitchtool.exe'
 #subprocess.call([appPath])
 #
 #
+stdoutdata = subprocess.getoutput(appPath+' Help')
+print (stdoutdata)
+
 #get serial number and unit model for making switch calls.
 stdoutdata = subprocess.getoutput(appPath+' getSwInfo')
 
-print ("units found: "+ stdoutdata)
-#get the values into variabls. this code below assumes there is only one unit , check returned data if you have more than one switch
-modelNum, serialNum= stdoutdata.split(',')[0],stdoutdata.split(',')[1]
-print("setting unit 1 : "+modelNum+' '+serialNum)
+try:
+    print ("units found: "+ stdoutdata)
+    #get the values into variabls. this code below assumes there is only one unit , check returned data if you have more than one switch
+    modelNum, serialNum= stdoutdata.split(',')[0],stdoutdata.split(',')[1]
+    print("setting unit 1 : "+modelNum+' '+serialNum)
 
+except:
+    print(" no switches found! make sure they are plugged in to this system and lights are on\nThe switch should cycle through all switches if it is connected and working")
+    sys.exit()
 #subprocess.call([appPath, 'setSwitch1','MY641410001' ,'U1816E' ,'5' ,'4']);
 
-swPos1='5';
-swPos2='4';
-print(f'setting switches on {serialNum}')
-#subprocess.call([appPath, 'setSwitch1',serialNum ,modelNum ,swPos1,swPos2]);
-stdoutdata = subprocess.getoutput(appPath+' setSwitch1 '+serialNum +' '+modelNum +' '+swPos1+' '+swPos2)
-swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
-print(f'Switch1= {swPos1} Switch2= {swPos2}')
+try:
+    swPos1='5';
+    swPos2='4';
+    print(f'setting switches on {serialNum}')
+    #subprocess.call([appPath, 'setSwitch1',serialNum ,modelNum ,swPos1,swPos2]);
+    stdoutdata = subprocess.getoutput(appPath+' setSwitch1 '+serialNum +' '+modelNum +' '+swPos1+' '+swPos2)
+    swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
+    print(f'Switch1= {swPos1} Switch2= {swPos2}')
 
-swPos1='1';
-swPos2='2';
-print(f'setting switches on {serialNum}')
-#subprocess.call([appPath, 'setSwitch1',serialNum ,modelNum ,swPos1,swPos2]);
-stdoutdata = subprocess.getoutput(appPath+' setSwitch1 '+serialNum +' '+modelNum +' '+swPos1+' '+swPos2)
-swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
-print(f'Switch1= {swPos1} Switch2= {swPos2}')
-
+    swPos1='1';
+    swPos2='2';
+    print(f'setting switches on {serialNum}')
+    #subprocess.call([appPath, 'setSwitch1',serialNum ,modelNum ,swPos1,swPos2]);
+    stdoutdata = subprocess.getoutput(appPath+' setSwitch1 '+serialNum +' '+modelNum +' '+swPos1+' '+swPos2)
+    swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
+    print(f'Switch1= {swPos1} Switch2= {swPos2}')
+except:
+    print("cant set switch, possibly not found?")
 
 try:
     stdoutdata = subprocess.getoutput(appPath + ' getSwInfo')
@@ -58,7 +69,9 @@ except:
 
 
 
-
-stdoutdata = subprocess.getoutput(appPath+' getSwitch1 '+serialNum +' '+modelNum )
-swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
-print(f'Getting Switch info:{modelNum} {serialNum} Switch1= {swPos1} Switch2= {swPos2}')
+try:
+    stdoutdata = subprocess.getoutput(appPath+' getSwitch1 '+serialNum +' '+modelNum )
+    swPos1,swPos2=stdoutdata.split(',')[0],stdoutdata.split(',')[1]
+    print(f'Getting Switch info:{modelNum} {serialNum} Switch1= {swPos1} Switch2= {swPos2}')
+except:
+    print("cant read switch, check if switch is powered and connected this system")
