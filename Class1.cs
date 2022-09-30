62813b03bc955bd1b596bf8a5198655f377df880
Tim F <fairdogJR@users.noreply.github.com>
2022-09-29 22:52:48 -0400

    updated some code cleanup

--- Class1.cs
+++ Class1.cs
@@ -7,23 +7,7 @@ using Agilent.U1816x;
 
 namespace u1816eNS
 {
-    public class unitData
-    {
-        public unitData(string serialno, string modelno)
-        {
-            this.serialno = serialno;
-            this.modelno = modelno;
 
-        }
-        public string serialno { get; private set; }
-        public string modelno { get; private set; }
-
-        
-
-
-
-
-    }
     public class initSwitch
 
     {
@@ -49,12 +33,7 @@ namespace u1816eNS
             //status = driver.RFPATH(a, b);
 
             driver.GetRFPath2(ref RFPath1, ref RFPath2);
-            
-            //get the instrument liste and serial numbers available
-           // driver.InstrumentList().ForEach(i => Console.WriteLine("{0}\t", i));
 
-            //Console.WriteLine("Switch 1 current RF path :     {0} ", RFPath1);
-            // Console.WriteLine("Switch 2 current RF path :     {0} \n", RFPath2);
             this.currentPath = RFPath1.ToString() + ',' + RFPath2.ToString();
         }
         public void setSwitch1(string serialNo, string modelNo, int swPos1, int swPos2)
@@ -64,10 +43,7 @@ namespace u1816eNS
             AgU1816x.StatusEnum status;
             int RFPath1 = 0, RFPath2 = 0;
             int CycleCount = 0;
-            
 
-            //string serialNo = "MY641410001"; //tkf this MUST be correct for the switch you are accessing
-           //string modelNo = "U1816E";
 
             driver.Initialize(modelNo, serialNo);
 // status = driver.RFPATH(a, b);
@@ -75,8 +51,7 @@ namespace u1816eNS
 
 
             driver.GetRFPath2(ref RFPath1, ref RFPath2);
-            //Console.WriteLine("Switch 1 current RF path :     {0} ", RFPath1);
-            //Console.WriteLine("Switch 2 current RF path :     {0} \n", RFPath2);
+
             this.currentPath = RFPath1.ToString() + ',' + RFPath2.ToString();
 
         }
@@ -91,23 +66,14 @@ namespace u1816eNS
             AgU1816x.StatusEnum status;
             int RFPath1 = 0, RFPath2 = 0;
             int CycleCount = 0;
-            //string unitInfo = "";
 
-
-            //driver.Initialize(modelNo, serialNo);
-            //status = driver.RFPATH(a, b);
-
-            //driver.GetRFPath2(ref RFPath1, ref RFPath2);
-
-            //get the instrument liste and serial numbers available
-            //driver.InstrumentList().ForEach(i => Console.Write("{0}\t", i));
 
             driver.InstrumentList().ForEach(i => this.unitInfo=i);
 
             foreach (var item in driver.InstrumentList())
             {
                 unitdata2.Add(item);
-              Console.Write(item+",");
+              Console.Write(item+",");// is is required for python to see data coming back using subprocess
 
             }  
 
